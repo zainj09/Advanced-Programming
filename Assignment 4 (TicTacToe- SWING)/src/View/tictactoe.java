92d4d9e9c9c589cca9c17a948b1d37f55ef18c6b
MUHAMMAD ZAIN JAFFERY <joinzain@gmail.com>
2019-03-24 14:06:09 +0500

    ScreenShots added

--- Assignment 4 (TicTacToe- SWING)/src/View/tictactoe.java
+++ Assignment 4 (TicTacToe- SWING)/src/View/tictactoe.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class tictactoe extends JFrame {
 
@@ -38,9 +39,9 @@ public class tictactoe extends JFrame {
         this.add(board);
         buttonAction();
         restartAction();
-        reset.setText("Restart Game");
+        reset.setText("Restart");
         textField.setEditable(false);
-        info.setLayout(new FlowLayout(1,70,20));
+        info.setLayout(new FlowLayout(1,50,20));
         info.add(textField);
         info.add(reset);
         this.add(info);
@@ -68,13 +69,13 @@ public class tictactoe extends JFrame {
                             ((JButton) e.getSource()).setEnabled(false);
 
                             if(checkWinner()==true){
-                                textField.setText("Game Over!");
+                                textField.setText("Submitted by Zain Jaffery");
                                 JOptionPane.showMessageDialog(null, "Game over! Player "+winner+" won");
                                 disableButtons();
                             }
 
                             else if(checkDraw()==true){
-                                textField.setText("Game Over!");
+                                textField.setText("Submitted by Zain Jaffery");
                                 JOptionPane.showMessageDialog(null, "Game over! Its a draw");
                                 disableButtons();
                             }
@@ -104,6 +105,7 @@ public class tictactoe extends JFrame {
                     for(int j=0;j<size;j++) {
                         buttons[i][j].setEnabled(true);
                         buttons[i][j].setText("");
+                        buttons[i][j].setBackground(null);
                         textField.setText("Take First Turn");
                     }
                 }
@@ -118,6 +120,8 @@ public class tictactoe extends JFrame {
             if (buttons[i][0].getText() == buttons[i][1].getText() && buttons[i][1].getText() == buttons[i][2].getText() && buttons[i][0].getText()!="") {
                 winner = buttons[i][0].getText();
                 count++;
+                for(int k=0;k<size;k++)
+                    buttons[i][k].setBackground(Color.BLUE);
             }
         }
         //for colmns(i.e verticl directn)...
@@ -125,16 +129,24 @@ public class tictactoe extends JFrame {
             if (buttons[0][j].getText() == buttons[1][j].getText() && buttons[1][j].getText() == buttons[2][j].getText() && buttons[1][j].getText()!=""){
                 winner = buttons[0][j].getText();
                 count++;
+                for(int k=0;k<size;k++)
+                    buttons[k][j].setBackground(Color.BLUE);
             }
         }
         //for diognals...
         if (buttons[0][0].getText() == buttons[1][1].getText() && buttons[1][1].getText() == buttons[2][2].getText() && buttons[2][2].getText()!="") {
             winner = buttons[0][0].getText();
             count++;
+            for(int i=0;i<size;i++)
+                buttons[i][i].setBackground(Color.BLUE);
         }
+
         if (buttons[0][2].getText() == buttons[1][1].getText() && buttons[1][1].getText() == buttons[2][0].getText() && buttons[2][0].getText()!="") {
             winner = buttons[0][2].getText();
             count++;
+            buttons[0][2].setBackground(Color.BLUE);
+            buttons[1][1].setBackground(Color.BLUE);
+            buttons[2][0].setBackground(Color.BLUE);
         }
         if (count == 0) {
             return false;
